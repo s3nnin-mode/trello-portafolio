50bcbc5771799bb37bb3670b5dbe2f03f5fd57ae
s3nnin-mode <holaleyder@gmail.com>
2025-03-08 21:42:18 -0800

    se agregó funcion para confirmar contraseña

--- src/components/rutas/formRegister.tsx
+++ src/components/rutas/formRegister.tsx
@@ -11,14 +11,16 @@ interface Props {
 
 export const FormRegister = () => {
   const navigate = useNavigate();
-
   const { 
     register, 
     handleSubmit, 
+    watch,
     formState: { errors } 
   } = useForm({
     mode: "onChange" // o "onChange" para validación en cada pulsación
   });
+
+  const password = watch('password');
 
   const onSubmit = (data: any) => {
     console.log('btn submit clicked', data)
@@ -30,52 +32,7 @@ export const FormRegister = () => {
     navigate('/');
   };
 
-  const [open, setOpen] = React.useState(false);
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
   return (
-    // <form className='form_register' onSubmit={handleSubmit(onSubmit)}>
-    //   <h1>Registro</h1>
-    //   <TextField
-    //     id='email'
-    //     label="Correo"
-    //     type="email"
-    //     variant="outlined"
-    //     defaultValue=''
-    //     {...register('email', { 
-    //       required: { value: true, message: 'El correo es obligatorio'}, 
-    //       pattern: { 
-    //         value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, 
-    //         message: 'Correo inválido'
-    //       } 
-    //     })}
-    //     error={!!errors.email} // Muestra el error si existe
-    //     helperText={errors.email ? String(errors.email.message) : '' } // Muestra el mensaje de error
-    //   />
-      
-    //   <TextField
-    //     id='password'
-    //     label='Contraseña'
-    //     type='password'
-    //     variant='outlined'
-    //     {...register('password', { required: { value: true, message: 'La contraseña es obligatoria'} })}
-    //     error={!!errors.password}
-    //     helperText={errors.password ? String(errors.password?.message) : ''}
-    //   />
-
-    //   <button type='submit'>Registrarme</button>
-    //   <div className=''>
-    //     <p>¿Ya tienes una cuenta? <Link to='/login'>Inicia sesión</Link></p>
-    //   </div>
-    // </form>
-
     <Paper elevation={3} sx={{ padding: 4, margin: "auto" }}>
       <Typography variant="h5" fontWeight="bold" textAlign="center" mb={2}>
         Registro
@@ -110,6 +67,20 @@ export const FormRegister = () => {
             })}
             error={!!errors.password}
             helperText={errors.password ? String(errors.password?.message) : ''}
+            fullWidth
+          />
+
+          <TextField
+            id="confirmPassword"
+            label="Confirmar contraseña"
+            type="password"
+            variant="outlined"
+            {...register("confirmPassword", { 
+              required: "Debes confirmar tu contraseña",
+              validate: (value) => value === password || 'las contraseñas no coinciden'
+            })}
+            error={!!errors.confirmPassword}
+            helperText={errors.confirmPassword ? String(errors.confirmPassword?.message) : ''}
             fullWidth
           />
 
