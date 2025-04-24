d76e4a408b1f3de7f3d48a69e8836cf94d6c9390
s3nnin-mode <holaleyder@gmail.com>
2025-04-24 12:22:28 -0700

    haciendo responsive opciones de lista

--- src/components/reusables/modalToRemoveItem.tsx
+++ src/components/reusables/modalToRemoveItem.tsx
@@ -1,84 +1,99 @@
 import '../../styles/components/reusables/modalToRemoveItem.scss';
-import { Modal, Button } from "react-bootstrap";
 import { CardProps, ListProps } from "../../types/boardProps";
 import { useListsServices } from '../../services/listsServices';
 import { useCardsServices } from "../../services/cardsServices";
 import { useAuthContext } from '../../customHooks/useAuthContext';
 import { deleteListFirebase } from '../../services/firebase/updateData/updateLists';
 import { deleteCard } from '../../services/firebase/updateData/updateCards';
+import { Backdrop, Box, Modal } from '@mui/material';
+import { IoMdClose } from 'react-icons/io';
 
 interface ModalRemoveListProps {
-    show: boolean
-    onHide: () => void
-    idBoard: string
-    list: ListProps
-    card?: CardProps
-    itemToRemove: 'card' | 'list'
+  show: boolean
+  onHide: () => void
+  idBoard: string
+  list: ListProps
+  card?: CardProps
+  itemToRemove: 'card' | 'list'
 }
 
 //AGREGAR ANIMACION DE ELIMINACION SUAVES
 
 export const ModalToRemoveItem: React.FC<ModalRemoveListProps> = ({show, onHide, card, list, idBoard, itemToRemove}) => {
-    const { listsService } = useListsServices();
-    const { cardsServices } = useCardsServices();
-    const { userAuth } = useAuthContext();
+  const { listsService } = useListsServices();
+  const { cardsServices } = useCardsServices();
+  const { userAuth } = useAuthContext();
 
-    const handleRemoveList = () => {
-        const idList = list.idList;
-        if (userAuth) {
-            deleteListFirebase({idBoard, idList});
+  const handleRemoveList = () => {
+    const idList = list.idList;
+    if (userAuth) {
+      deleteListFirebase({idBoard, idList});
+    }
+    listsService({
+      updateFn: (state) => state.map((listGroup) => 
+      listGroup.idBoard === idBoard ?
+      {...listGroup, lists: listGroup.lists.filter(list => list.idList !== idList)} :
+      listGroup
+      )
+    });
+    onHide();
+  }
+
+  const handleRemoveCard = () => {
+    const idCardToRemove = card?.idCard;
+    if (userAuth && card) {
+      deleteCard({
+        idBoard,
+        idList: list.idList,
+        idCard: card?.idCard
+      });
+    }
+
+    cardsServices({
+      updateFn: (cardsGroup) => cardsGroup.map((cardGroup) =>
+      (cardGroup.idBoard === idBoard && cardGroup.idList === list.idList) ? 
+      {...cardGroup, cards: cardGroup.cards.filter((card) => card.idCard !== idCardToRemove)} :
+      cardGroup
+      )
+    });
+  }
+
+  const actions = {
+    list: handleRemoveList,
+    card: handleRemoveCard
+  }
+
+  return (
+    <Modal
+      sx={{
+        minHeight: '100vh',
+        overflowY: 'auto',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'        
+      }}
+      aria-labelledby="transition-modal-title"
+      aria-describedby="transition-modal-description"
+      open={show}
+      onClose={onHide}
+      closeAfterTransition
+      slots={{ backdrop: Backdrop }}
+      slotProps={{
+        backdrop: {
+          timeout: 350,
+        },
+      }}
+    >
+      <Box className='modal_to_remove_item'>
+        <IoMdClose onClick={onHide} className='icon_close_modal_to_remove_item' />
+        {itemToRemove === 'list'
+        ? <p className='roboto'>¿Estás seguro de que quieres eliminar la lista <span>"{list.nameList}"?</span></p>
+        : <p className='roboto'>¿Estás seguro de que quieres eliminar la tarjeta <span>"{card?.nameCard}"?</span></p>
         }
-        listsService({
-            updateFn: (state) => state.map((listGroup) => 
-            listGroup.idBoard === idBoard ?
-            {...listGroup, lists: listGroup.lists.filter(list => list.idList !== idList)} :
-            listGroup
-            )
-        });
-        onHide();
-    }
-
-    const handleRemoveCard = () => {
-        const idCardToRemove = card?.idCard;
-        if (userAuth && card) {
-            deleteCard({
-                idBoard,
-                idList: list.idList,
-                idCard: card?.idCard
-            });
-        }
-        cardsServices({
-            updateFn: (cardsGroup) => cardsGroup.map((cardGroup) =>
-            (cardGroup.idBoard === idBoard && cardGroup.idList === list.idList) ? 
-            {...cardGroup, cards: cardGroup.cards.filter((card) => card.idCard !== idCardToRemove)} :
-            cardGroup
-            )
-        })
-    }
-
-    const actions = {
-        list: handleRemoveList,
-        card: handleRemoveCard
-    }
-
-    return (
-        <Modal className='modal_to_remove_item' show={show} onHide={onHide} centered>
-            <Modal.Header closeButton>
-            </Modal.Header>
-            <Modal.Body style={{textAlign: 'center'}}>
-            {
-                itemToRemove === 'list'
-                ?
-                <p>Estas seguro de que quieres eliminar la lista <span>"{list.nameList}"?</span></p>
-                :
-                <p>Estas seguro de que quieres eliminar la tarjeta <span>"{card?.nameCard}"?</span></p>
-            }
-            </Modal.Body>
-            <Modal.Footer>
-                <Button variant="danger" onClick={actions[itemToRemove]}>       
-                    Remove
-                </Button>
-            </Modal.Footer>
-        </Modal>
-    )
+        <button className='inter' onClick={actions[itemToRemove]}>       
+          Eliminar
+        </button>
+      </Box>
+    </Modal>
+  )
 };
